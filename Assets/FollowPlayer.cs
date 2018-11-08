a73b221b3e95684fc6ef24cdcee5a24252cd0c8c
Geurtsy <>
2018-11-08 11:39:13 +0800

    VR works yoohoo

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour 
+{
+    [SerializeField] Transform cameraRigTransform;
+    [SerializeField] GameObject holsters;
+
+    private void Update() 
+    {
+        holsters.transform.position = cameraRigTransform.position;
+
+        Quaternion quart = Quaternion.identity;
+
+        quart.eulerAngles = new Vector3(0, cameraRigTransform.rotation.eulerAngles.y, 0);
+
+     //   holsters.transform.rotation = quart;
+    }
+}
