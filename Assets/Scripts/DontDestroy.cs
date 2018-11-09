441fc06e4a8121890cd92cf81d741412cfd92fbf
Laurence <101215@student.sae.edu.au>
2018-11-09 10:26:18 +0800

    AudioManager partially completed - SFX logic implemented
    Still need to implement music and soundscape logic using snapshots

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// This script can be attached to any game object that you wish to remain active between scenes.
+/// </summary>
+
+public class DontDestroy : MonoBehaviour
+{
+    private void Awake()
+    {
+        // do not destroy this gameobject when a new scene is loaded
+        DontDestroyOnLoad(this.gameObject);
+
+
+        // if more than one of this type of object exists
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            // destroy the gameobject
+            Destroy(gameObject);
+        }
+    }
+}
