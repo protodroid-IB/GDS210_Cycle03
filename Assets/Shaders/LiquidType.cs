01c443dfe7cb14d5567ca81773fb49f6928360ab
Kimberley Smith <1007073@student.sae.edu.au>
2018-11-12 10:51:13 +0800

    shader progress

---
+++ Assets/Shaders/LiquidType.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Liquids/New Liquid")]
+public class LiquidType : ScriptableObject {
+
+	public float MaxWobble = 0.03f;
+	public float WobbleSpeed = 1f;
+	public float Recovery = 1f;
+	public float wobbleSmooth;
+	public Color liquidColor;
+	public Color foamColor;
+	public float maxFill, minFill;
+}
