3f188edd27ee0ad1b34ad9773eb79df66ad139b4
Laurence <101215@student.sae.edu.au>
2018-11-20 15:36:36 +0800

    fixed texture rendering of my character / changed outside soundscape and implemented as a prefab
    To use just make sure AudioManager and Soundscape Prefabs are in the scene.

--- Assets/Audio/Scripts/Soundscapes.cs
+++ Assets/Audio/Scripts/Soundscapes.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Soundscapes : MonoBehaviour
 {
 
-    [SerializeField] private AudioSource Outside1AS, Outside2AS, Outside3AS, InsideAS;
+    [SerializeField] private AudioSource[] outsideAudioSources;
+    [SerializeField] private AudioSource insideAudioSource;
 
 	// Use this for initialization
 	void Start ()
     {
-        AudioManager.instance.PlaySound("Soundscape_Outside", ref Outside1AS);
-        AudioManager.instance.PlaySound("Soundscape_Outside", ref Outside2AS);
-        AudioManager.instance.PlaySound("Soundscape_Outside", ref Outside3AS);
-        AudioManager.instance.PlaySound("Soundscape_Inside", ref InsideAS);
+        for(int i=0; i < outsideAudioSources.Length; i++)
+        {
+            AudioManager.instance.PlaySound("Soundscape_Outside", ref outsideAudioSources[i]);
+        }
+        
+        AudioManager.instance.PlaySound("Soundscape_Inside", ref insideAudioSource);
     }
 }
