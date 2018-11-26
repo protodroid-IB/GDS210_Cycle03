6833bc6fab1e831081110a68a84afd86e027ddaf
johnv1010715 <40983263+johnv1010715@users.noreply.github.com>
2018-11-26 17:15:53 +0800

    Added Cactus Guy And Some Other Stuff
    + Nameless Cactus Guy
    + Cactus Textures and Materials
    + Cactus Animations
    + Big Rocks
    + Big Rocks Materials

---
+++ Assets/Scripts/NPC/UILookAt.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILookAt : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject player;
+
+    Transform playerPos;
+
+	void Start ()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if(player == null)
+        {
+
+            Debug.Log("PC Player not found. Using VR Player. " + player.name);
+            player = GameObject.FindGameObjectWithTag("VRPayer");
+
+        }
+        else
+        {
+
+            Debug.Log("PC Player found! You can use a VR HMD for a more immersive experience! " + player.name);
+
+        }
+
+        playerPos = player.transform;
+
+	}
+	
+	void Update ()
+    {
+
+        transform.LookAt(playerPos);
+
+	}
+}
