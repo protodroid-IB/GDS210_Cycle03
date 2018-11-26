f1fd162d7ce496caa8226834a1d0090ac14dfffc
BlakeShortland <40289871+BlakeShortland@users.noreply.github.com>
2018-11-26 12:54:03 +0800

    Throwing game points added

---
+++ Assets/Scripts/ThrowingGame/DidIHitTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DidIHitTarget : MonoBehaviour
+{
+    ThrowingGameController tgc;
+
+	void Awake ()
+    {
+        tgc = GameObject.Find("ThrowingGameController").GetComponent<ThrowingGameController>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "ThrowingTarget")
+            tgc.AddScore(10);
+    }
+}
---
+++ Assets/Scripts/ThrowingGame/ThrowingGameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ThrowingGameController : MonoBehaviour
+{
+    public int score;
+
+    [SerializeField] TMP_Text scoreText;
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+		
+	}
+
+    public void AddScore(int points)
+    {
+        score += points;
+        scoreText.text = "Score: " + score.ToString("0000");
+    }
+}
