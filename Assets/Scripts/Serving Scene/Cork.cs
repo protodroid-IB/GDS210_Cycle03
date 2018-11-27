b7fd4d35cb371515a3491693184459062a62d1d6
Kimberley Smith <1007073@student.sae.edu.au>
2018-11-27 15:13:48 +0800

    started making labels

---
+++ Assets/Scripts/Serving Scene/Cork.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Cork : MonoBehaviour {
+
+	[SerializeField]
+	GameObject corkObject;
+
+	[SerializeField]
+	Transform corkTransform;
+
+	// Use this for initialization
+	void Start () {
+		GameObject cork = Instantiate(corkObject, corkTransform.position, corkTransform.rotation);
+		cork.transform.localScale *= 9.5f;
+		FixedJoint joint = cork.GetComponent<FixedJoint>();
+		joint.connectedBody = GetComponent<Rigidbody>();
+	}
+
+}
