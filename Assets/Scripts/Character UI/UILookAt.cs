6c289793e2f3fd6aac1e1885e2a2f1aaf7f7c293
johnv1010715 <40983263+johnv1010715@users.noreply.github.com>
2018-11-29 10:36:40 +0800

    Cactus Guy Implemented

---
+++ Assets/Scripts/Character UI/UILookAt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILookAt : MonoBehaviour
+{
+
+    GameObject player;
+
+    void Start ()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        if(player == null)
+        {
+
+            player = GameObject.FindGameObjectWithTag("VRPlayer");
+
+        }
+
+	}
+	
+	void Update ()
+    {
+
+        transform.LookAt(player.transform.position);
+
+	}
+
+}
