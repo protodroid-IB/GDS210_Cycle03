b20f1e720fcef1f8fde7688ae28797902496362a
BlakeShortland <40289871+BlakeShortland@users.noreply.github.com>
2018-12-03 15:17:40 +0800

    Throwing Game Update
    -Has game loop

--- Assets/Scripts/ThrowingGame/ThrowingGameController.cs
+++ Assets/Scripts/ThrowingGame/ThrowingGameController.cs
@@ -6,28 +6,86 @@ using TMPro;
 public class ThrowingGameController : MonoBehaviour
 {
     GameManager gameManager;
+    
     public int score;
+    public float timeLimit;
+    public float timeRemaining;
 
     [SerializeField] TMP_Text scoreText;
+    [SerializeField] TMP_Text timerText;
+
+    [SerializeField] GameObject knivesContainer;
+    [SerializeField] GameObject axesContainer;
+
+    GameObject activeKnivesContainer;
+    GameObject activeAxesContainer;
+
+
+    public bool freeplayMode;
 
 	void Start ()
     {
         gameManager = FindObjectOfType<GameManager>();
+
+        freeplayMode = false;
     }
 
 	void Update ()
     {
-		
+        timerText.text = "Time Remaining: " + timeRemaining;
 	}
 
     public void AddScore(int points)
     {
-        score += points;
-        scoreText.text = "Score: " + score.ToString("0000");
+        if (!freeplayMode)
+        {
+            score += points;
+            scoreText.text = "Score: " + score.ToString("0000");
+        }
     }
 
     public void RestartMiniGame(string minigame)
     {
         gameManager.RestartMiniGame(minigame);
+    }
+
+    public void StartFreeplay()
+    {
+        freeplayMode = true;
+        StartThrowingGame(freeplayMode);
+    }
+
+    public void StartTimedMode()
+    {
+        freeplayMode = false;
+        StartThrowingGame(freeplayMode);
+    }
+
+    void StartThrowingGame(bool freeplaymode)
+    {
+        if (activeKnivesContainer != null)
+            Destroy(activeKnivesContainer);
+
+        if (activeAxesContainer != null)
+            Destroy(activeAxesContainer);
+
+        activeKnivesContainer = Instantiate(knivesContainer);
+        activeAxesContainer = Instantiate(axesContainer);
+
+        if (!freeplayMode)
+        {
+            timeRemaining = timeLimit;
+
+            while(timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+
+            if (timeRemaining <= 0)
+            {
+                timeRemaining = 0;
+                freeplayMode = true;
+            }
+        }
     }
 }
