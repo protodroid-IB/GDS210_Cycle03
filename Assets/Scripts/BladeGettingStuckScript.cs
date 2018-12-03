3fde099dc421076a50180b4d3174cbe968c5474f
kenstudent <40884999+kenstudent@users.noreply.github.com>
2018-12-03 10:08:28 +0800

    throwing game blade sticks
    blades should stick to everything now with a fixed joint.
    
    swinging targets cylinder have a rigidbody which the blades will stick to.
    
    changed weapons table mesh collider to everything
    
    changed target boards collider to have the throwing target tag.
    
    added throwing game menu to scene.

--- Assets/Scripts/BladeGettingStuckScript.cs
+++ Assets/Scripts/BladeGettingStuckScript.cs
@@ -11,6 +11,14 @@ public class BladeGettingStuckScript : MonoBehaviour
     ThrowingGameController tgc;
     DidIHitTarget diht;
 
+    FixedJoint myFixedJoint;
+    
+    [Tooltip("How deep the blade will stick into a target.")]
+    [SerializeField] float depth;
+
+    [Tooltip("Force required to break the FixedJoint")]
+    [SerializeField] int breakForce;
+
     private void Awake()
     {
         myObjectRB = myObject.GetComponent<Rigidbody>();
@@ -26,36 +34,53 @@ public class BladeGettingStuckScript : MonoBehaviour
         {
             return;
         }
-        
-        myObjectRB.constraints = RigidbodyConstraints.FreezeAll;
+
 
         if (other.gameObject.tag == "ThrowingTarget")
         {
-            ParentMe(other);
+            ParentMe(other, depth);     // Sticks to targets, and penetrates.
 
             if (!diht.AmIUsed())
             {
+                print(other.name);
                 diht.SetUsed();
                 tgc.AddScore(100);
             }
             
         }
-    }
-
-    private void OnTriggerExit(Collider collision)
-    {
-        if (collision.gameObject.layer == 2)
+        else
         {
-            return;
+            // Sticks to any other collider but does not penetrates. Stops falling through floor.
+            // Enables kinematic to stop parents collider bouncing away from others collider.
+            ParentMe(other, 0);    
+            myObjectRB.isKinematic = true;
         }
 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Ensure the rigidbody is not kinematic when pulled away from other colliders.
+        myObjectRB.isKinematic = false;
+    }
+
+    private void ParentMe(Collider other, float penetrateDepth)
+    {
+
+        // Stick the blade into the target, using depth as a measure to how far it sticks in.
+        myObject.transform.Translate(myObjectRB.velocity.normalized * Time.deltaTime * -penetrateDepth);
+
+
+        // Adjust Rigidbody to account for being stuck in a target, especially the moving target.
+        myObjectRB.velocity = Vector3.zero;
+        myObjectRB.useGravity = false;
         myObjectRB.constraints = RigidbodyConstraints.None;
 
+        // Create the fixed joint.
+        myFixedJoint = myObject.AddComponent<FixedJoint>();
+        myFixedJoint.connectedBody = other.GetComponent<Rigidbody>();
+        myFixedJoint.breakForce = breakForce;
+
     }
 
-    private void ParentMe(Collider other)
-    {
-        myObject.GetComponent<Transform>().SetParent(other.gameObject.transform, true);
-        myObject.transform.localScale = other.gameObject.transform.localScale;
-    }
 }
---
+++ Assets/Scripts/ThrowingGame/BreakFixedJoint.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This should be added to the knives/axes object that has a rigidbody.
+/// 
+/// When the fixed joint is broken, the rigidbody has to return to its original settings.
+/// </summary>
+
+[RequireComponent(typeof(Rigidbody))]
+public class BreakFixedJoint : MonoBehaviour {
+    Rigidbody myObjectRB;
+
+    private void Start()
+    {
+        myObjectRB = GetComponent<Rigidbody>();
+    }
+
+
+    void OnJointBreak(float breakForce)
+    {
+        Debug.Log("A joint from " + gameObject.name + " has just been broken!, force: " + breakForce);
+
+        BreakJoint();
+    }
+
+    public void BreakJoint()
+    {
+
+        myObjectRB.useGravity = true;
+        myObjectRB.constraints = RigidbodyConstraints.FreezeRotationY;
+        myObjectRB.isKinematic = false;
+    }
+
+
+}
