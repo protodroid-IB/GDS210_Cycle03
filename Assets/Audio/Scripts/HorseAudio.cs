18d1c0071dc16232e15ccca1bb8015d497562851
Laurence <101215@student.sae.edu.au>
2018-12-04 08:39:37 +0800

    added bird and horse noises seperate from soundscape

---
+++ Assets/Audio/Scripts/HorseAudio.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseAudio : MonoBehaviour
+{
+    // 0 = neigh
+    // 1 = gallop
+    private AudioSource[] audioSources;
+
+    private Animator animator; 
+
+    [SerializeField]
+    private bool isGalloping = false;
+
+    private float minNeighTime = 15f;
+    private float maxNeighTime = 60f;
+
+    private float neighTime;
+    private float neighTimer = 0f;
+
+    // the amount of semitones the pitch may vary from the original.
+    // if the origanl was a D and min and max were -2 and 2 respectively the range would be
+    // bwetween C and E
+    private float minSemitone = -2f;
+    private float maxSemitone = 2f;
+    private float newPitch = 1f;
+
+    private bool walking = false;
+
+    private const float PITCH_MULT = 1.05946f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        audioSources = GetComponents<AudioSource>();
+        animator = GetComponent<Animator>();
+
+        newPitch = Mathf.Pow(PITCH_MULT, Random.Range(minSemitone, maxSemitone));
+        neighTime = Random.Range(minNeighTime, maxNeighTime);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (neighTimer >= neighTime)
+        {
+            newPitch = Mathf.Pow(PITCH_MULT, Random.Range(minSemitone, maxSemitone));
+            neighTime = Random.Range(minNeighTime, maxNeighTime);
+            neighTimer = 0f;
+
+
+            Debug.Log(animator.GetBool("walking"));
+
+            
+            AudioManager.instance.PlaySound("HorseNeigh", ref audioSources[0], newPitch, 1f);
+            
+        }
+        else
+            neighTimer += Time.deltaTime;
+
+        GallopSound();
+    }
+
+
+
+    private void GallopSound()
+    {
+        if(isGalloping)
+        {
+            if (animator.GetBool("walking"))
+            {
+                if (walking == false)
+                {
+                    AudioManager.instance.PlaySound("HorseGallop", ref audioSources[1]);
+                    walking = true;
+                }
+            }
+            else
+            {
+                audioSources[1].Stop();
+                walking = false;
+            }
+        }
+        
+    }
+
+
+
+
+}
---
+++ Assets/Audio/Scripts/IntermittentlyPlaySound.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class IntermittentlyPlaySound : MonoBehaviour
+{
+    [SerializeField]
+    private string soundName;
+
+    [SerializeField]
+    private float minPlayTime = 15f, maxPlayTime = 60f;
+
+    private float playTime;
+    private float playTimer = 0f;
+
+
+    [Space(5)]
+    [Header("The amount of semitones the pitch may vary from the original. If the originl was a D and min and max were -2 and 2 respectively the range would be bwetween C and E")]
+    [SerializeField]
+    private float minSemitone = -2f;
+    [SerializeField]
+    private float maxSemitone = 2f;
+
+    private float newPitch = 1f;
+
+    private const float PITCH_MULT = 1.05946f;
+
+    private AudioSource thisAudio;
+
+    // Use this for initialization
+    void Start ()
+    {
+        thisAudio = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (playTimer >= playTime)
+        {
+            newPitch = Mathf.Pow(PITCH_MULT, Random.Range(minSemitone, maxSemitone));
+            playTime = Random.Range(minPlayTime, maxPlayTime);
+            playTimer = 0f;
+
+            AudioManager.instance.PlaySound(soundName, ref thisAudio, newPitch);
+        }
+        else
+            playTimer += Time.deltaTime;
+    }
+}
