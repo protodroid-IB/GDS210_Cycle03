1d17e659add19c1b5a37b4a931506752823c41fa
kenstudent <40884999+kenstudent@users.noreply.github.com>
2018-12-08 00:09:50 +0800

    flattened horse
    made some new materials for the horse for variety.
    
    also fixed that damned hat glitching

---
+++ Assets/Scripts/BigCowBoyDrink.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class BigCowBoyDrink : MonoBehaviour {
+    Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+        myAnimator = GetComponent<Animator>();
+        myAnimator.speed = Random.Range(0.950f, 1.105f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            DrinkUp();
+        }
+
+    }
+
+    public void DrinkUp()
+    {
+        myAnimator.SetTrigger("Drink");
+        myAnimator.speed = Random.Range(0.950f, 1.105f);
+    }
+}
--- Assets/Scripts/ThrowingGame/DidIHitTarget.cs
+++ Assets/Scripts/ThrowingGame/DidIHitTarget.cs
@@ -47,7 +47,7 @@ public class DidIHitTarget : MonoBehaviour
 
     public bool AmIUsed()
     {
-        print(used);
+
         if (used)
             return true;
         else
