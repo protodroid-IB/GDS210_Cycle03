54dff202df801087aa19e58ec9e82a47464188c1
Geurtsy <>
2018-12-07 15:49:05 +0800

    Should be done

---
+++ Assets/Scripts/Hubworld/TutorialObjects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialObjects : MonoBehaviour {
+    [SerializeField] GameSettings gameSettings;
+    
+    [SerializeField] Transform[] children;
+	
+	// Update is called once per frame
+	void Update () {
+        
+        print(gameObject.activeSelf);
+	}
+}
